51e13566ea7c5c43e4728f7ea6bcda2d150d2cca
“Romer” <romeralvareziv@gmail.com>
2023-02-21 18:59:37 +0000

    Character model implemented

---
+++ src/API/models/Character.ts
@@ -0,0 +1,42 @@
+import { Schema, Document, Model, model } from "mongoose";
+
+const characterSchema: Schema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  universe: {
+    type: String,
+    required: true,
+  },
+  realName: {
+    type: String,
+    required: true,
+  },
+  powers: [String],
+  origin: {
+    type: String,
+    enum: ["Natural", "Artificial", "Cosmic", "Mutant", "Other", "Unknown"],
+    default: "Unknown",
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  }
+});
+
+export interface CharacterModel extends Document {
+  name: string;
+  description: string;
+  universe: string;
+  realName: string;
+  powers: string[];
+  origin: string;
+  created: Date;
+}
+
+export const Character: Model<CharacterModel> = model<CharacterModel>("Character", characterSchema);
